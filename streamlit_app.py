1f5b543b847e783020d98c432b6c275d3fef8636
kelvin paschal <kelvinidanwekhai@gmail.com>
2022-01-24 11:35:48 +0100

    third

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,27 @@
 import streamlit as st
-import py3Dmol
 from stmol import showmol
-st.sidebar.title('Show Proteins')
-prot_str='1A2C,1BML,1D5M,1D5X,1D5Z,1D6E,1DEE,1E9F,1FC2,1FCC,1G4U,1GZS,1HE1,1HEZ,1HQR,1HXY,1IBX,1JBU,1JWM,1JWS'
-prot_list=prot_str.split(',')
-bcolor = st.sidebar.color_picker('Pick A Color', '#00f900')
-protein=st.sidebar.selectbox('select protein',prot_list)
-style = st.sidebar.selectbox('style',['line','cross','stick','sphere','cartoon','clicksphere'])
-spin = st.sidebar.checkbox('Spin', value = False)
-xyzview = py3Dmol.view(query='pdb:'+protein)
-xyzview.setStyle({style:{'color':'spectrum'}})
-xyzview.setBackgroundColor(bcolor)
-if spin:
-    xyzview.spin(True)
-else:
-    xyzview.spin(False)
-xyzview.zoomTo()
-showmol(xyzview,height=500,width=800)
+import py3Dmol
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+st.title('RDKit + Py3DMOL 😀')
+
+def makeblock(smi):
+    mol = Chem.MolFromSmiles(smi)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    mblock = Chem.MolToMolBlock(mol)
+    return mblock
+
+def render_mol(xyz):
+    xyzview = py3Dmol.view()#(width=400,height=400)
+    xyzview.addModel(xyz,'mol')
+    xyzview.setStyle({'stick':{}})
+    xyzview.setBackgroundColor('white')
+    xyzview.zoomTo()
+    showmol(xyzview,height=500,width=500)
+
+compound_smiles=st.text_input('SMILES please','CC')
+blk=makeblock(compound_smiles)
+render_mol(blk)
